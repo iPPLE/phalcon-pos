b86e3bbf8c92aa3eb3172db0f48803e37e8d4d9d
ipple <seth12n13@gmail.com>
2014-09-18 17:16:23 +0700

    first commit

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+    	echo "<h1>Hello from webtools!</h1>";
+    }
+
+}
+
+/*class IndexController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+        echo "<h1>Hello!</h1>";
+    }
+
+}*/
+
